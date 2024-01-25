19ac1ad77446b8b8df869931de8bab65b4fc04cd
unknown <vyacheslav.listopad@gmail.com>
2024-01-25 17:48:35 +0300

    work  text smaller

--- pages/work/index.js
+++ pages/work/index.js
@@ -62,7 +62,7 @@ const Work = () => {
             І чим далі ти розвиваєшся - тим вище заробітня плата  .
           </p>
         </div>
-        <div className="w-full xl:max-w-[68%]">
+        <div className="w-full xl:max-w-[68%] mt-10">
           {/*slider */}
           <WorkSlider />
         </div>
