c4c6f3fdd3d111bcb183a5690a0a7b100d8e0e51
unknown <vyacheslav.listopad@gmail.com>
2024-01-25 17:38:08 +0300

    work  text smaller

--- pages/services/index.js
+++ pages/services/index.js
@@ -50,7 +50,7 @@ const Services = () => {
         <div className="text-center flex xl:w-[36vw] flex-col lg:text-left mb-4 xl:mb-0">
           <h2 className="text-3xl xl:mt-8"> <span className="text-accent">Наш </span> офіс</h2>
           <p className="mb-4 max-w-[400px] text-sm font-bold text-md mx-auto lg:mx-0">
-          Стимулююче робоче середовище: У нас панує атмосфера, яка надихає на досягнення результатів, а кожен день приносить нові виклики .
+          Стимулююче робоче середовище:<br /> У нас панує атмосфера, яка надихає на досягнення результатів, а кожен день приносить нові виклики .
         </p>
         </div>
         <div className="w-full xl:max-w-[68%]">
